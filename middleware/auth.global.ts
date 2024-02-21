5115003de95f527067855a9615bd48101c75b41c
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2024-02-21 14:03:29 +0200

    asdad

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -6,47 +6,67 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   const cookie = useCookie("mtud");
 
   if (!to.path.includes("pasiulymas")) {
+    console.log("1");
     if (process.client) {
+      console.log("2");
       if (!cookie.value) {
+        console.log("3");
         if (to.path !== "/login") {
+          console.log("4");
           useUser.logout();
           return navigateTo("/login");
+          console.log("5");
         }
       }
-
+      console.log("6");
       if (!useUser.user) {
+        console.log("7");
         const { data }: any = await useFetch("/api/auth", {
           method: "post",
           body: {},
         });
+        console.log("8");
 
         if (!data?.value?.data) {
+          console.log("9");
           if (to.path !== "/login") {
+            console.log("10");
             useUser.logout();
+            console.log("11");
             return navigateTo("/login");
           }
         }
+        console.log("12");
         useUser.setUser({ ...data.value.data });
       }
     }
 
     if (process.server) {
+      console.log("13");
       if (cookie.value) {
+        console.log("14");
         const { data }: any = await useFetch("/api/auth", {
           method: "post",
           body: {},
         });
+        console.log("15");
         if (!data?.value?.data) {
+          console.log("16");
           if (to.path !== "/login") {
             useUser.logout();
+            console.log("17");
             return navigateTo("/login");
           }
+          console.log("18");
         } else {
+          console.log("19");
           useUser.setUser({ ...data?.value?.data });
         }
       } else {
+        console.log("20");
         if (to.path !== "/login") {
           useUser.logout();
+          console.log("21");
           return navigateTo("/login");
         }
       }
--- server/api/auth.ts
+++ server/api/auth.ts
@@ -2,6 +2,7 @@ import type { User } from "~/data/interfaces";
 
 export default defineEventHandler(async (event) => {
   const { _id } = await readBody(event);
+  console.log("22");
 
   if (!_id)
     return {
@@ -11,6 +12,7 @@ export default defineEventHandler(async (event) => {
     };
 
   const data: User | null = await userSchema.findById({ _id });
+  console.log("23");
 
   if (!data)
     return {
@@ -19,6 +21,7 @@ export default defineEventHandler(async (event) => {
       message: "Vartotojas nerastas",
     };
 
+  console.log("24");
   data.password = "";
 
   return { success: true, data: data, message: "Vartotojas prijungtas" };
