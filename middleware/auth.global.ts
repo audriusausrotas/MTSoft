0d0116f74ac41603adb4cf2f6e561a8438bfebde
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2024-02-21 14:29:56 +0200

    aaa

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -8,34 +8,24 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
   if (!to.path.includes("pasiulymas")) {
     if (process.client) {
       if (!cookie.value) {
-        console.log("3");
         if (to.path !== "/login") {
-          console.log("4");
           useUser.logout();
           return navigateTo("/login");
         }
       }
-      console.log(cookie);
-      console.log(cookie.value);
       if (cookie.value && !useUser.user) {
-        console.log("7");
         const { data }: any = await useFetch("/api/auth", {
           method: "post",
           body: {},
         });
-        console.log("8");
 
         if (!data?.value?.data) {
-          console.log("9");
           if (to.path !== "/login") {
-            console.log("10");
             useUser.logout();
-            console.log("11");
             return navigateTo("/login");
           }
         }
-        console.log("12");
-        useUser.setUser({ ...data?.value?.data });
+        useUser.setUser({ ...data.value.data });
       }
     }
 
@@ -46,19 +36,17 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
           body: {},
         });
 
-        if (!data?.value?.data) {
+        if (!data.value.data) {
           if (to.path !== "/login") {
             useUser.logout();
-
             return navigateTo("/login");
           }
         } else {
-          useUser.setUser({ ...data?.value?.data });
+          useUser.setUser({ ...data.value.data });
         }
       } else {
         if (to.path !== "/login") {
           useUser.logout();
-
           return navigateTo("/login");
         }
       }
