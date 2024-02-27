2008e825b7e6a71307bb7f063bac4113ecfc0b20
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2024-02-27 11:37:47 +0200

    asdasd

--- server/api/project.post.ts
+++ server/api/project.post.ts
@@ -1,78 +1,79 @@
 import { projectSchema } from "~/server/models/projectSchema";
 
 export default defineEventHandler(async (event) => {
-  try {
-    const {
-      client,
-      fenceMeasures,
-      results,
-      creator,
-      works,
-      gates,
-      totalPrice,
-      totalCost,
-      totalProfit,
-      totalMargin,
-      priceVAT,
-      priceWithDiscount,
-      discount,
-      confirmed,
-      payed,
-      status,
-      advance,
-    } = await readBody(event);
+  const {
+    client,
+    fenceMeasures,
+    results,
+    creator,
+    works,
+    gates,
+    totalPrice,
+    totalCost,
+    totalProfit,
+    totalMargin,
+    priceVAT,
+    priceWithDiscount,
+    discount,
+    confirmed,
+    payed,
+    status,
+    advance,
+  } = await readBody(event);
 
-    const currentDate = new Date();
+  const currentDate = new Date();
 
-    const dateCreated = currentDate.toISOString();
+  const dateCreated = currentDate.toISOString();
 
-    let expirationDate = new Date(currentDate);
-    expirationDate.setDate(currentDate.getDate() + 30);
+  let expirationDate = new Date(currentDate);
+  expirationDate.setDate(currentDate.getDate() + 30);
 
-    const dateExparation = expirationDate.toISOString();
+  const dateExparation = expirationDate.toISOString();
 
-    const firstThreeLetters = creator.username.substring(0, 3).toUpperCase();
+  const firstThreeLetters = creator.username.substring(0, 3).toUpperCase();
 
-    const userProjects = await projectSchema.find({ creator });
+  const userProjects = await projectSchema.find({ creator });
 
-    const lastOrder = userProjects[userProjects.length - 1].orderNumber;
+  const lastOrder = userProjects[userProjects.length - 1].orderNumber;
 
+  let newOrderNumbers = "";
+
+  if (!lastOrder) {
+    newOrderNumbers = "0001";
+  } else {
     let orderNumbers = parseInt(lastOrder.split("-")[1]);
     orderNumbers++;
 
-    const newOrderNumbers = orderNumbers.toString().padStart(4, "0");
-
-    const orderNumber = `${firstThreeLetters}-${newOrderNumbers}`;
-
-    const project = new projectSchema({
-      creator,
-      client,
-      fenceMeasures,
-      results,
-      orderNumber,
-      works,
-      gates,
-      totalPrice,
-      totalCost,
-      totalProfit,
-      totalMargin,
-      priceVAT,
-      priceWithDiscount,
-      discount,
-      confirmed,
-      payed,
-      status,
-      advance,
-      dateCreated,
-      dateExparation,
-    });
-
-    const data = await project.save();
-
-    data._id = data._id.toString();
-
-    return { success: true, data: data, message: "Projektas išsaugotas" };
-  } catch (error) {
-    return { success: false, data: null, message: error };
+    newOrderNumbers = orderNumbers.toString().padStart(4, "0");
   }
+  const orderNumber = `${firstThreeLetters}-${newOrderNumbers}`;
+
+  const project = new projectSchema({
+    creator,
+    client,
+    fenceMeasures,
+    results,
+    orderNumber,
+    works,
+    gates,
+    totalPrice,
+    totalCost,
+    totalProfit,
+    totalMargin,
+    priceVAT,
+    priceWithDiscount,
+    discount,
+    confirmed,
+    payed,
+    status,
+    advance,
+    dateCreated,
+    dateExparation,
+  });
+
+  const data = await project.save();
+
+  data._id = data._id.toString();
+
+  return { success: true, data: data, message: "Projektas išsaugotas" };
 });
