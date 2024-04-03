425cbfb8fbba40407fca94bf79c0bf51f3e3f050
Audrius Ausrotas <audrius.ausrotas@gmail.com>
2024-04-03 23:36:53 +0300

    asdafasd

--- server/api/gamyba.post.ts
+++ server/api/gamyba.post.ts
@@ -20,64 +20,147 @@ export default defineEventHandler(async (event) => {
   } else {
     const bindings: Bindings[] = [];
 
+    const addBindings = (color: string, height: number, type: string) => {
+      let found = false;
+      const quantity = type === "Centrinis" || type === "Elka" ? 2 : 1;
+
+      for (const binding of bindings) {
+        if (
+          binding.color === color &&
+          binding.height === height &&
+          binding.type === type
+        ) {
+          binding.quantity += quantity;
+          found = true;
+          break;
+        }
+      }
+
+      if (!found) {
+        bindings.push({
+          color,
+          height,
+          type,
+          quantity: quantity,
+        });
+      }
+      found = false;
+    };
+
+    const front: any = [];
+    const left: any = [];
+    const back: any = [];
+    const right: any = [];
+
     project.fenceMeasures.forEach((item) => {
-      if (item.bindings) {
+      const bindingItem = {
+        bindings: item.bindings === "Taip" ? true : false,
+        color: item.color,
+        firstHeight: item.measures[0].height,
+        lastHeight: item.measures[item.measures.length - 1].height,
+      };
+
+      if (item.direction === "Priekis") front.push(bindingItem);
+      if (item.direction === "Kairė") left.push(bindingItem);
+      if (item.direction === "Galas") back.push(bindingItem);
+      if (item.direction === "Dešinė") right.push(bindingItem);
+
+      if (item.bindings === "Taip") {
         let lastHeight = 0;
         let stepHeight = 0;
         let stepDirection = "";
-        let step = false;
 
         item.measures.forEach((measure, index) => {
           if (measure.laiptas.exist) {
             stepDirection = measure.laiptas.direction;
             stepHeight = measure.laiptas.value;
-            step = true;
             return;
           }
 
           const color = item.color;
           let height = measure.height;
           const type = measure.kampas.exist
-            ? "kampinis" + measure.kampas.value
-            : "centrinis";
+            ? "Kampinis" + measure.kampas.value
+            : measure.gates.exist
+            ? "Elka"
+            : "Centrinis";
 
-          // dadet logika kur lygini buvusi su esamu ir pasirenki didesni
-          if (step) {
-            lastHeight = measure.height;
-            step = false;
+          if (lastHeight > height) {
+            height = lastHeight;
+            if (stepDirection === "Žemyn") height += stepHeight;
+          } else if (lastHeight < height) {
+            if (stepDirection === "Aukštyn") height += stepHeight;
           } else {
-            lastHeight = measure.height;
+            height += stepHeight;
           }
 
-          console.log(index, height);
+          stepHeight = 0;
+          stepDirection = "";
+          lastHeight = measure.height;
 
           if (index !== 0) {
-            let found = false;
-            for (const binding of bindings) {
-              if (
-                binding.color === color &&
-                binding.height === height &&
-                binding.type === type
-              ) {
-                binding.quantity += type === "centrinis" ? 2 : 1;
-                found = true;
-                break;
-              }
-            }
-
-            if (!found) {
-              bindings.push({
-                color,
-                height,
-                type,
-                quantity: type === "centrinis" ? 2 : 1,
-              });
-            }
-            found = false;
+            addBindings(color, height, type);
           }
         });
       }
     });
+
+    //istestuot
+    const calculateMiddle = (array: any) => {
+      if (array.length > 1) {
+        let lastElement: any;
+
+        for (const item of array) {
+          if (lastElement) {
+            if (lastElement.bingings) {
+              if (item.bindings) {
+                if (
+                  lastElement.lastHeight === item.firstHeight ||
+                  lastElement.lastHeight > item.firstHeight
+                ) {
+                  addBindings(
+                    lastElement.color,
+                    lastElement.lastHeight,
+                    "Centrinis"
+                  );
+                } else {
+                  addBindings(item.color, item.firstHeight, "Centrinis");
+                }
+              } else {
+                addBindings(lastElement.color, lastElement.lastHeight, "Elka");
+              }
+            } else {
+              if (item.bindings) {
+                addBindings(item.color, item.firstHeight, "Elka");
+              }
+            }
+            lastElement = item;
+          } else {
+            lastElement = item;
+          }
+        }
+      }
+    };
+
+    calculateMiddle(front);
+    calculateMiddle(left);
+    calculateMiddle(back);
+    calculateMiddle(right);
+
+    //  const front = [];
+    // const left = [];
+    // const back = [];
+    // const right = [];
+
+    // project.fenceMeasures.forEach((item) => {
+    //   const bindingItem = {
+    //     bindings: item.bindings,
+    //     color: item.color,
+    //     firstHeight: item.measures[0].height,
+    //     lastHeight: item.measures[item.measures.length - 1].height,
+    //   };
+
+    const calculateCorners = () => {};
 
     console.log(bindings);
 
