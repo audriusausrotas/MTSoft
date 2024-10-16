c2825e124642b6de17206bde2eac815ec074d131
Audrius <audrius.ausrotas@gmail.com>
2024-10-16 23:46:31 +0300

    test2

--- ecosystem.config.cjs
+++ ecosystem.config.cjs
@@ -4,7 +4,7 @@ module.exports = {
         name: 'MTSoft',
         port: '3000',
         exec_mode: 'fork',
-        instances: 'max',
+        instances: '1',
         script: './.output/server/index.mjs',
         env_production: {
           NODE_ENV: 'production',
--- server/api/webhook.post.ts
+++ server/api/webhook.post.ts
@@ -5,24 +5,25 @@ import { exec } from 'child_process';
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
   console.log("veikia?")
-console.log(body)
-  // // Check if the action is a push
-  // if (body.action === 'push') {
-  //   return new Promise((resolve, reject) => {
-  //     // Execute the deployment commands
-  //     exec('git pull && npm install && npm run build && pm2 restart MTSoft', { cwd: 'C:/MTwebsite/MTSoft' }, (error, stdout, stderr) => {
-  //       if (error) {
-  //         console.error(`Error: ${error.message}`);
-  //         return reject(createError({ statusCode: 500, statusMessage: 'Deployment failed' }));
-  //       }
-  //       if (stderr) {
-  //         console.error(`Stderr: ${stderr}`);
-  //       }
-  //       console.log(`Stdout: ${stdout}`);
-  //       return resolve({ message: 'Deployment successful' });
-  //     });
-  //   });
-  // }
+console.log("action" + body.action)
+  // Check if the action is a push
+  if (body.action === 'push') {
+    return new Promise((resolve, reject) => {
+      // Execute the deployment commands
+      console.log("executina")
+      exec('git pull && npm install && npm run build && pm2 restart MTSoft', { cwd: 'C:/MTwebsite/MTSoft' }, (error, stdout, stderr) => {
+        if (error) {
+          console.error(`Error: ${error.message}`);
+          return reject(createError({ statusCode: 500, statusMessage: 'Deployment failed' }));
+        }
+        if (stderr) {
+          console.error(`Stderr: ${stderr}`);
+        }
+        console.log(`Stdout: ${stdout}`);
+        return resolve({ message: 'Deployment successful' });
+      });
+    });
+  }
 
   return { status: 'success' };
 });
