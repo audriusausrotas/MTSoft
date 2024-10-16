6bad59c9662707b24fcaa490099fa819c172d5e8
Audrius <audrius.ausrotas@gmail.com>
2024-10-16 23:37:58 +0300

    testing webhook

--- server/api/webhook.post.ts
+++ server/api/webhook.post.ts
@@ -1,27 +1,28 @@
 // server/middleware/webhook.js
 import { exec } from 'child_process';
 
+
 export default defineEventHandler(async (event) => {
   const body = await readBody(event);
-
-  // Check if the action is a push
-  if (body.action === 'push') {
-    return new Promise((resolve, reject) => {
-      // Execute the deployment commands
-      exec('git pull && npm install && npm run build && pm2 restart MTSoft', { cwd: 'C:/MTwebsite/MTSoft' }, (error, stdout, stderr) => {
-      
-        if (error) {
-          console.error(`Error: ${error.message}`);
-          return reject(createError({ statusCode: 500, statusMessage: 'Deployment failed' }));
-        }
-        if (stderr) {
-          console.error(`Stderr: ${stderr}`);
-        }
-        console.log(`Stdout: ${stdout}`);
-        return resolve({ message: 'Deployment successful' });
-      });
-    });
-  }
+  console.log("veikia?")
+console.log(body)
+  // // Check if the action is a push
+  // if (body.action === 'push') {
+  //   return new Promise((resolve, reject) => {
+  //     // Execute the deployment commands
+  //     exec('git pull && npm install && npm run build && pm2 restart MTSoft', { cwd: 'C:/MTwebsite/MTSoft' }, (error, stdout, stderr) => {
+  //       if (error) {
+  //         console.error(`Error: ${error.message}`);
+  //         return reject(createError({ statusCode: 500, statusMessage: 'Deployment failed' }));
+  //       }
+  //       if (stderr) {
+  //         console.error(`Stderr: ${stderr}`);
+  //       }
+  //       console.log(`Stdout: ${stdout}`);
+  //       return resolve({ message: 'Deployment successful' });
+  //     });
+  //   });
+  // }
 
   return { status: 'success' };
 });
