2cdae50098e4ef87f36a98ecbf359c60517a5847
Audrius <audrius.ausrotas@gmail.com>
2024-10-17 00:46:57 +0300

    asdf

--- pages/index.vue
+++ pages/index.vue
@@ -31,7 +31,7 @@ const getBackup = async () => {
         <BaseButton>
           <NuxtLink to="/naujas">Naujas projektas</NuxtLink>
         </BaseButton>
-
+        <div>.</div>
         <BaseInput
           placeholder="Paieška"
           label="Paieška"
