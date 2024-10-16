cc1dc883dd921afb2f2018d964df89aa1922618f
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2024-10-16 10:05:57 +0300

    aaa

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -4,10 +4,6 @@ export default defineNuxtRouteMiddleware(async (to) => {
   const useUser = useUserStore();
   const cookie = useCookie("mtud");
 
-  if (to.path === "/api/webhook") {
-    return; 
-  }
-
   if (!to.path.includes("pasiulymas")) {
     // authentificate in server from cookie
     if (cookie.value) {
