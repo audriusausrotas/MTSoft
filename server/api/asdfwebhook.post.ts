b5ac179f9cef8920faa7b1c6d57293fd8732c850
Audrius <audrius.ausrotas@gmail.com>
2024-10-17 20:42:43 +0300

    asdas

--- server/api/asdfwebhook.post.ts
+++ server/api/asdfwebhook.post.ts
@@ -1,32 +1,3 @@
-// // server/middleware/webhook.js
-// import { exec } from 'child_process';
-
-// export default defineEventHandler(async (event) => {
-//   const body = await readBody(event);
-//   console.log(body);
-//   console.log("Webhook triggered");
-
-//   // Check if the ref is pointing to the main branch
-//   if (body.ref === 'refs/heads/main') {
-//     return new Promise((resolve, reject) => {
-//       console.log("Running deployment commands...");
-//       exec('git pull && npm install && npm run build && pm2 restart MTSoft', { cwd: 'C:/MTwebsite/MTSoft' }, (error, stdout, stderr) => {
-//         if (error) {
-//           console.error(`Error: ${error.message}`);
-//           return reject(createError({ statusCode: 500, statusMessage: 'Deployment failed' }));
-//         }
-//         if (stderr) {
-//           console.error(`Stderr: ${stderr}`);
-//         }
-//         console.log(`Stdout: ${stdout}`);
-//         return resolve({ message: 'Deployment successful' });
-//       });
-//     });
-//   }
-
-//   return { status: 'success' };
-// });
-
 import { exec } from 'child_process';
 
 export default defineEventHandler(async (event) => {
@@ -53,7 +24,7 @@ export default defineEventHandler(async (event) => {
       console.log(`Stdout: ${stdout}`);
     });
 
-    return; // Stop further execution of the handler
+    return; 
   }
 console.log("iveike")
   return { status: 'success' };
