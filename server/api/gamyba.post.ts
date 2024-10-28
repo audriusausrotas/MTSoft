baf40660769e14e43e454b462f1d6c4515cc82a3
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2024-10-28 15:55:54 +0200

    asdasd

--- server/api/gamyba.post.ts
+++ server/api/gamyba.post.ts
@@ -84,114 +84,124 @@ export default defineEventHandler(async (event) => {
 
         if (!isBindings) {
           addBindings(color, measure.height, "Koja Dviguba " + legWidth, 2);
-        }
-
-        if (index === 0) {
-          if (notSpecial) {
-            lastHeight = measure.height;
-            addBindings(color, measure.height, "Koja vienguba " + legWidth, 1);
-          }
-          return;
-        }
-
-        if (index === item.measures.length - 1) {
-          if (notSpecial)
-            addBindings(color, measure.height, "Koja vienguba " + legWidth, 1);
-        }
-
-        if (measure.gates.exist) {
-          if (index !== 0 && !wasGates) {
-            const maxHeight = Math.max(lastHeight, measure.height);
-
-            addBindings(color, maxHeight, "Koja vienguba " + legWidth, 1);
-            isBindings && addBindings(color, maxHeight, "Elka", 2);
-          }
-          wasGates = true;
-          if (index === 0) lastHeight = measure.height;
-        } else if (measure.kampas.exist) {
-          cornerRadius = measure.kampas.value;
-          wasCorner = true;
-        } else if (measure.laiptas.exist) {
-          stepDirection = measure.laiptas.direction;
-          stepHeight = measure.laiptas.value;
-          wasStep = true;
         } else {
-          if (wasGates) {
-            const maxHeight = Math.max(lastHeight, measure.height);
+          if (index === 0) {
+            if (notSpecial) {
+              lastHeight = measure.height;
+              addBindings(
+                color,
+                measure.height,
+                "Koja vienguba " + legWidth,
+                1
+              );
+            }
+            return;
+          }
 
-            addBindings(color, maxHeight, "Koja vienguba " + legWidth, 1);
+          if (index === item.measures.length - 1) {
+            if (notSpecial)
+              addBindings(
+                color,
+                measure.height,
+                "Koja vienguba " + legWidth,
+                1
+              );
+          }
 
-            isBindings && addBindings(color, maxHeight, "Elka", 2);
+          if (measure.gates.exist) {
+            if (index !== 0 && !wasGates) {
+              const maxHeight = Math.max(lastHeight, measure.height);
 
-            wasGates = false;
-            lastHeight = measure.height;
-          } else if (wasCorner && wasStep) {
-            const maxHeight =
-              stepDirection === "Aukštyn"
-                ? lastHeight + stepHeight - (lastHeight - measure.height)
-                : measure.height + stepHeight - (measure.height - lastHeight);
-
-            isBindings &&
-              addBindings(color, maxHeight, "Kampas " + cornerRadius, 1);
-
-            addBindings(color, maxHeight, "Koja vienguba " + legWidth, 1);
-            addBindings(
-              color,
-              stepDirection === "Aukštyn" ? measure.height : lastHeight,
-              "Koja vienguba " + legWidth,
-              1
-            );
-
-            wasCorner = false;
-            wasStep = false;
-            lastHeight = measure.height;
-          } else if (wasCorner) {
-            const maxHeight = Math.max(lastHeight, measure.height);
-            isBindings &&
-              addBindings(color, maxHeight, "Kampas " + cornerRadius, 1);
-
-            addBindings(
-              color,
-              maxHeight,
-              "Koja vienguba " + legWidth,
-
-              2
-            );
-
-            wasCorner = false;
-            lastHeight = measure.height;
-          } else if (wasStep) {
-            const maxHeight =
-              stepDirection === "Aukštyn"
-                ? lastHeight + stepHeight - (lastHeight - measure.height)
-                : measure.height + stepHeight - (measure.height - lastHeight);
-
-            isBindings && addBindings(color, maxHeight, "Centrinis", 2);
-
-            addBindings(color, maxHeight, "Koja vienguba " + legWidth, 1);
-
-            addBindings(
-              color,
-              stepDirection === "Aukštyn" ? measure.height : lastHeight,
-              "Koja vienguba " + legWidth,
-              1
-            );
-            wasStep = false;
-            lastHeight = measure.height;
+              addBindings(color, maxHeight, "Koja vienguba " + legWidth, 1);
+              isBindings && addBindings(color, maxHeight, "Elka", 2);
+            }
+            wasGates = true;
+            if (index === 0) lastHeight = measure.height;
+          } else if (measure.kampas.exist) {
+            cornerRadius = measure.kampas.value;
+            wasCorner = true;
+          } else if (measure.laiptas.exist) {
+            stepDirection = measure.laiptas.direction;
+            stepHeight = measure.laiptas.value;
+            wasStep = true;
           } else {
-            const maxHeight = Math.max(lastHeight, measure.height);
+            if (wasGates) {
+              const maxHeight = Math.max(lastHeight, measure.height);
 
-            isBindings && addBindings(color, maxHeight, "Centrinis", 2);
+              addBindings(color, maxHeight, "Koja vienguba " + legWidth, 1);
 
-            addBindings(
-              color,
-              maxHeight,
-              "Koja vienguba " + legWidth,
+              isBindings && addBindings(color, maxHeight, "Elka", 2);
 
-              2
-            );
+              wasGates = false;
+              lastHeight = measure.height;
+            } else if (wasCorner && wasStep) {
+              const maxHeight =
+                stepDirection === "Aukštyn"
+                  ? lastHeight + stepHeight - (lastHeight - measure.height)
+                  : measure.height + stepHeight - (measure.height - lastHeight);
 
-            lastHeight = measure.height;
+              isBindings &&
+                addBindings(color, maxHeight, "Kampas " + cornerRadius, 1);
+
+              addBindings(color, maxHeight, "Koja vienguba " + legWidth, 1);
+              addBindings(
+                color,
+                stepDirection === "Aukštyn" ? measure.height : lastHeight,
+                "Koja vienguba " + legWidth,
+                1
+              );
+
+              wasCorner = false;
+              wasStep = false;
+              lastHeight = measure.height;
+            } else if (wasCorner) {
+              const maxHeight = Math.max(lastHeight, measure.height);
+              isBindings &&
+                addBindings(color, maxHeight, "Kampas " + cornerRadius, 1);
+
+              addBindings(
+                color,
+                maxHeight,
+                "Koja vienguba " + legWidth,
+
+                2
+              );
+
+              wasCorner = false;
+              lastHeight = measure.height;
+            } else if (wasStep) {
+              const maxHeight =
+                stepDirection === "Aukštyn"
+                  ? lastHeight + stepHeight - (lastHeight - measure.height)
+                  : measure.height + stepHeight - (measure.height - lastHeight);
+
+              isBindings && addBindings(color, maxHeight, "Centrinis", 2);
+
+              addBindings(color, maxHeight, "Koja vienguba " + legWidth, 1);
+
+              addBindings(
+                color,
+                stepDirection === "Aukštyn" ? measure.height : lastHeight,
+                "Koja vienguba " + legWidth,
+                1
+              );
+              wasStep = false;
+              lastHeight = measure.height;
+            } else {
+              const maxHeight = Math.max(lastHeight, measure.height);
+
+              isBindings && addBindings(color, maxHeight, "Centrinis", 2);
+
+              addBindings(
+                color,
+                maxHeight,
+                "Koja vienguba " + legWidth,
+
+                2
+              );
+
+              lastHeight = measure.height;
+            }
           }
         }
       });
