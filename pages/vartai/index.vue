b444d8cc8cc0a9a08aece3837bd02eb65063eb3e
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2024-12-20 10:38:00 +0200

    added searh in gates

--- pages/vartai/index.vue
+++ pages/vartai/index.vue
@@ -4,15 +4,33 @@ const useUser = useUserStore();
 
 const filteredGates = computed(() => {
   if (useUser.user!.accountType === "Administratorius") {
-    return useGates.gates;
+    return useGates.filteredGates;
   } else {
-    return useGates.gates.filter((item) => item.manager === useUser.user?.email);
+    return useGates.filteredGates.filter(
+      (item) => item.manager === useUser.user?.email
+    );
   }
 });
 </script>
 
 <template>
   <div class="flex flex-col gap-4">
+    <BaseInput
+      placeholder="Paieška"
+      width="flex-1"
+      variant="light"
+      @onChange="(value: string): void => useGates.searchGates(value)"
+    >
+      <NuxtImg
+        src="/icons/search.svg"
+        width="14"
+        height="14"
+        alt="search icon"
+        decoding="auto"
+        loading="lazy"
+        :ismap="true"
+      />
+    </BaseInput>
     <div
       v-for="(gate, index) in filteredGates"
       :key="gate._id"
--- store/gates.ts
+++ store/gates.ts
@@ -3,21 +3,25 @@ import type { GateSchema } from "~/data/interfaces";
 export const useGateStore = defineStore("gate", {
   state: () => ({
     gates: [] as GateSchema[],
+    filteredGates: [] as GateSchema[],
   }),
 
   actions: {
     addGates(data: GateSchema[]) {
       this.gates = [...data];
+      this.updateFilteredGates();
     },
 
     removeGates(id: string) {
       if (this.gates.some((item) => item._id === id)) {
         this.gates = this.gates.filter((item) => item._id !== id);
       }
+      this.updateFilteredGates();
     },
 
     addGate(data: GateSchema) {
       this.gates.push(data);
+      this.updateFilteredGates();
     },
 
     addSelectedGate(data: GateSchema) {
@@ -30,6 +34,27 @@ export const useGateStore = defineStore("gate", {
         if (item._id === id) return data;
         else return item;
       });
+    },
+
+    searchGates(value: string) {
+      if (value.length > 2) {
+        const foundProjects = this.filteredGates.filter(
+          (project) =>
+            project.client.address
+              .toLowerCase()
+              .includes(value.toLowerCase()) ||
+            project.manager.toLowerCase().includes(value.toLowerCase()) ||
+            project.orderNr.toLowerCase().includes(value.toLowerCase())
+        );
+
+        this.filteredGates = [...foundProjects];
+      } else {
+        this.updateFilteredGates();
+      }
+    },
+
+    updateFilteredGates() {
+      this.filteredGates = [...this.gates];
     },
   },
 
