a43d0d2d0b692982bbc6de5e77a1ff6dfa8affb2
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2025-02-05 10:45:00 +0200

    still working on offers

--- components/Offers/FenceCard.vue
+++ components/Offers/FenceCard.vue
@@ -15,52 +15,134 @@ const props = defineProps([
 </script>
 
 <template>
-  <div class="flex flex-col gap-8 py-8 rounded-lg shadow-lg border items-center">
-    <div class="font-bold text-4xl text-center">{{ name }}</div>
-    <div class="flex gap-4 flex-wrap">
-      <div class="flex flex-col gap-4">
-        <div class="grid grid-cols-3 h-fit border rounded-md border-gray-500 min-w-fit">
-          <div class="px-2 py-1 border-b border-r border-inherit">Skardos tipas</div>
-          <div class="px-2 py-1 border-b border-inherit border-r font-bold">Premium</div>
-          <div class="px-2 py-1 border-b border-inherit font-bold">Eco</div>
-          <div class="px-2 py-1 border-b border-inherit border-r">Kaina be PVM</div>
-          <div class="px-2 py-1 border-b border-inherit border-r">{{ pp }} €</div>
-          <div class="px-2 py-1 border-b border-inherit">{{ ep }} €</div>
-          <div class="px-2 py-1 border-b border-inherit border-r">Kaina su PVM</div>
-          <div class="px-2 py-1 border-b border-inherit border-r">{{ ppv }} €</div>
-          <div class="px-2 py-1 border-b border-inherit">{{ epv }} €</div>
-          <div class="px-2 py-1 border-b border-inherit border-r flex">
-            m <span class="text-[12px] items-start mr-1">2</span>kaina be PVM
-          </div>
-          <div class="px-2 py-1 border-b border-inherit border-r">{{ spp }} €</div>
-          <div class="px-2 py-1 border-b border-inherit">{{ sep }} €</div>
-          <div class="px-2 py-1 flex border-r border-inherit">
-            m <span class="text-[12px] items-start mr-1">2</span>kaina su PVM
-          </div>
-          <div class="px-2 py-1 border-r border-inherit">{{ sppv }} €</div>
-          <div class="px-2 py-1">{{ sepv }} €</div>
+  <div
+    class="flex flex-col gap-8 p-8 rounded-lg shadow-lg border border-gray-300 items-center"
+  >
+    <div class="flex gap-4 flex-wrap justify-between w-full">
+      <div class="flex flex-col gap-1 flex-1 max-w-[430px]">
+        <div class="text-center font-bold text-xl">
+          {{ name }} - Nepramatoma
         </div>
-        <p>Į kainą įtraukti U formos profiliai skirti tvoros montavimui</p>
-        <p>cia dar galima prirasyt bendros info apie tvora</p>
+        <div
+          class="grid grid-cols-3 h-fit border-[3px] rounded-md border-black font-medium"
+        >
+          <div
+            class="px-2 py-1 border-b-[3px] border-r border-inherit font-bold"
+          >
+            Skardos tipas
+          </div>
+          <div
+            class="px-2 py-1 border-b-[3px] border-inherit border-r font-bold"
+          >
+            Premium
+          </div>
+          <div class="px-2 py-1 border-b-[3px] border-inherit font-bold">
+            Eco
+          </div>
+          <div class="px-2 py-1 border-b border-inherit border-r">
+            Kaina be PVM
+          </div>
+          <div class="px-2 py-1 border-b border-inherit border-r">
+            {{ pp }} €/m
+          </div>
+          <div class="px-2 py-1 border-b border-inherit">{{ ep }} €/m</div>
+          <div class="px-2 py-1 border-b-[3px] border-inherit border-r flex">
+            Kaina be PVM
+          </div>
+          <div class="px-2 py-1 border-b-[3px] border-inherit border-r flex">
+            {{ spp }} €/m <span class="text-[12px] items-start mr-1">2</span>
+          </div>
+          <div class="px-2 py-1 border-b-[3px] border-inherit flex">
+            {{ sep }} €/m <span class="text-[12px] items-start mr-1">2</span>
+          </div>
+          <div class="px-2 py-1 border-b border-inherit border-r">
+            Kaina su PVM
+          </div>
+          <div class="px-2 py-1 border-b border-inherit border-r">
+            {{ ppv }} €/m
+          </div>
+          <div class="px-2 py-1 border-b border-inherit">{{ epv }} €/m</div>
+          <div class="px-2 py-1 border-r border-inherit">Kaina su PVM</div>
+          <div class="px-2 py-1 border-r border-inherit flex">
+            {{ sppv }} €/m <span class="text-[12px] items-start mr-1">2</span>
+          </div>
+          <div class="px-2 py-1 flex">
+            {{ sepv }} €/m <span class="text-[12px] items-start mr-1">2</span>
+          </div>
+        </div>
+
+        <div class="text-center font-bold text-xl mt-2">
+          {{ name }} - Vidutinis pramatomumas
+        </div>
+        <div
+          class="grid grid-cols-3 h-fit border-2 rounded-md border-gray-500 min-w-fit"
+        >
+          <div
+            class="px-2 py-1 border-b-2 border-r border-inherit font-semibold"
+          >
+            Skardos tipas
+          </div>
+          <div
+            class="px-2 py-1 border-b-2 border-inherit border-r font-semibold"
+          >
+            Premium
+          </div>
+          <div class="px-2 py-1 border-b-2 border-inherit font-semibold">
+            Eco
+          </div>
+          <div class="px-2 py-1 border-b border-inherit border-r">
+            Kaina be PVM
+          </div>
+          <div class="px-2 py-1 border-b border-inherit border-r">
+            {{ pp }} €/m
+          </div>
+          <div class="px-2 py-1 border-b border-inherit">{{ ep }} €/m</div>
+          <div class="px-2 py-1 border-b-2 border-inherit border-r flex">
+            Kaina be PVM
+          </div>
+          <div class="px-2 py-1 border-b-2 border-inherit border-r flex">
+            {{ spp }} €/m <span class="text-[12px] items-start mr-1">2</span>
+          </div>
+          <div class="px-2 py-1 border-b-2 border-inherit flex">
+            {{ sep }} €/m <span class="text-[12px] items-start mr-1">2</span>
+          </div>
+          <div class="px-2 py-1 border-b border-inherit border-r">
+            Kaina su PVM
+          </div>
+          <div class="px-2 py-1 border-b border-inherit border-r">
+            {{ ppv }} €/m
+          </div>
+          <div class="px-2 py-1 border-b border-inherit">{{ epv }} €/m</div>
+          <div class="px-2 py-1 border-r border-inherit">Kaina su PVM</div>
+          <div class="px-2 py-1 border-r border-inherit flex">
+            {{ sppv }} €/m <span class="text-[12px] items-start mr-1">2</span>
+          </div>
+          <div class="px-2 py-1 flex">
+            {{ sepv }} €/m <span class="text-[12px] items-start mr-1">2</span>
+          </div>
+        </div>
       </div>
 
-      <div class="max-w-[230px]">
-        <NuxtImg
-          :src="`/images/${img1}.jpg`"
-          alt="Rombo tipo tvora maketas"
-          decoding="auto"
-          loading="lazy"
-          :ismap="true"
-        />
-      </div>
-      <div class="max-w-[600px] max-h-[100px]">
-        <NuxtImg
-          :src="`/images/${img2}.jpg`"
-          alt="Rombo tipo tvora"
-          decoding="auto"
-          loading="lazy"
-          :ismap="true"
-        />
+      <div class="flex gap-4">
+        <div class="h-[400px]">
+          <NuxtImg
+            :src="`/images/${img1}.jpg`"
+            alt="Rombo tipo tvora maketas"
+            decoding="auto"
+            loading="lazy"
+            :ismap="true"
+            class="h-full w-auto object-cover"
+          />
+        </div>
+        <div class="max-w-[600px] h-[400px]">
+          <NuxtImg
+            :src="`/images/${img2}.jpg`"
+            alt="Rombo tipo tvora"
+            decoding="auto"
+            loading="lazy"
+            :ismap="true"
+          />
+        </div>
       </div>
     </div>
   </div>
--- components/Offers/SteelInfo.vue
+++ components/Offers/SteelInfo.vue
@@ -1,34 +1,42 @@
 <template>
-  <div class="grid grid-cols-3 border rounded-lg shadow-lg">
-    <div class="px-2 py-1 border-b border-r">Skardos tipas</div>
-    <div class="px-2 py-1 border-b border-r font-bold">Premium</div>
-    <div class="px-2 py-1 border-b font-bold">Eco</div>
-    <div class="px-2 py-1 border-b border-r">Gamintojas</div>
-    <div class="px-2 py-1 border-b border-r">Thyssenkrupp</div>
-    <div class="px-2 py-1 border-b">Marcegaglia</div>
-    <div class="px-2 py-1 border-b border-r">Gamintojo šalis</div>
-    <div class="px-2 py-1 border-b border-r">Vokietija</div>
-    <div class="px-2 py-1 border-b">Italija</div>
-    <div class="px-2 py-1 border-b border-r">Garantija</div>
-    <div class="px-2 py-1 border-b border-r">45 metai</div>
-    <div class="px-2 py-1 border-b">5 metai</div>
-    <div class="px-2 py-1 border-b border-r">Skardos storis</div>
-    <div class="px-2 py-1 border-b border-r">0.5 mm</div>
-    <div class="px-2 py-1 border-b">0.45 mm</div>
-    <div class="px-2 py-1 border-b border-r">Skardos identiško atspalvio garantas</div>
-    <div class="px-2 py-1 border-b border-r">Taip</div>
-    <div class="px-2 py-1 border-b">Ne</div>
-    <div class="px-2 py-1 border-b border-r">Rekomenduojamas elemento ilgis</div>
-    <div class="px-2 py-1 border-b border-r">2500 mm</div>
-    <div class="px-2 py-1 border-b">2000 mm</div>
-    <div class="px-2 py-1 border-b border-r">Maksimalus elemento ilgis</div>
-    <div class="px-2 py-1 border-b border-r">3000 mm</div>
-    <div class="px-2 py-1 border-b">2500 mm</div>
-    <div class="px-2 py-1 border-b border-r">Metalinės dangos masė (g/m2)</div>
-    <div class="px-2 py-1 border-b border-r">ZM 130 (cinko, magnio mišinys)</div>
-    <div class="px-2 py-1 border-b">---</div>
-    <div class="px-2 py-1 border-r">Padengimas</div>
-    <div class="px-2 py-1 border-r">min. 35 mikronų dviejų sluoksnių dengimo sistema</div>
-    <div class="px-2 py-1">---</div>
+  <div class="grid grid-cols-3 border rounded-lg shadow-xl border-gray-300">
+    <div class="px-4 py-1 border-b border-r">Skardos tipas</div>
+    <div class="px-4 py-1 border-b border-r font-bold">Premium</div>
+    <div class="px-4 py-1 border-b font-bold">Eco</div>
+    <div class="px-4 py-1 border-b border-r">Gamintojas</div>
+    <div class="px-4 py-1 border-b border-r">Thyssenkrupp</div>
+    <div class="px-4 py-1 border-b">Marcegaglia</div>
+    <div class="px-4 py-1 border-b border-r">Gamintojo šalis</div>
+    <div class="px-4 py-1 border-b border-r">Vokietija</div>
+    <div class="px-4 py-1 border-b">Italija</div>
+    <div class="px-4 py-1 border-b border-r">Garantija</div>
+    <div class="px-4 py-1 border-b border-r">45 metai</div>
+    <div class="px-4 py-1 border-b">5 metai</div>
+    <div class="px-4 py-1 border-b border-r">Skardos storis</div>
+    <div class="px-4 py-1 border-b border-r">0.5 mm</div>
+    <div class="px-4 py-1 border-b">0.45 mm</div>
+    <div class="px-4 py-1 border-b border-r">
+      Skardos identiško atspalvio garantas
+    </div>
+    <div class="px-4 py-1 border-b border-r">Taip</div>
+    <div class="px-4 py-1 border-b">Ne</div>
+    <div class="px-4 py-1 border-b border-r">
+      Rekomenduojamas elemento ilgis
+    </div>
+    <div class="px-4 py-1 border-b border-r">2500 mm</div>
+    <div class="px-4 py-1 border-b">2000 mm</div>
+    <div class="px-4 py-1 border-b border-r">Maksimalus elemento ilgis</div>
+    <div class="px-4 py-1 border-b border-r">3000 mm</div>
+    <div class="px-4 py-1 border-b">2500 mm</div>
+    <div class="px-4 py-1 border-b border-r">Metalinės dangos masė (g/m2)</div>
+    <div class="px-4 py-1 border-b border-r">
+      ZM 130 (cinko, magnio mišinys)
+    </div>
+    <div class="px-4 py-1 border-b">---</div>
+    <div class="px-4 py-1 border-r">Padengimas</div>
+    <div class="px-4 py-1 border-r">
+      min. 35 mikronų dviejų sluoksnių dengimo sistema
+    </div>
+    <div class="px-4 py-1">---</div>
   </div>
 </template>
