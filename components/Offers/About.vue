ee8fb9d44fb7a08c20f8b111e95b24fd666899ea
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2025-02-10 14:35:55 +0200

    didmena styles and data updated

--- components/Offers/About.vue
+++ components/Offers/About.vue
@@ -3,113 +3,125 @@
 <template>
   <OffersBoxWrap>
     <div class="flex flex-col gap-4 md:gap-8">
-      <div class="flex gap-8 items-center">
-        <NuxtImg
-          src="/images/stipriausi.jpg"
-          alt="Rombo tipo tvora"
-          decoding="auto"
-          loading="lazy"
-          :ismap="true"
-          class="object-cover rounded-md self-center flex-1 w-full h-full"
-        />
-      </div>
-      <div class="flex gap-4 flex-wrap">
-        <div class="flex flex-col gap-4 flex-1 items-center justify-evenly">
-          <p class="text-justify font-normal md:max-w-96 min-w-72">
-            <span class="font-bold sm:text-xl text-lg">„Moderni Tvora“</span>
-            – šiuolaikiškus tvorų sprendimus siūlanti įmonė. Ilgametę patirtį turinti profesionalų
-            komanda specializuojasi skardinių tvorų gamyboje. Didelį dėmesį skiria gaminių kokybei
-            bei valdymo procesams, kurie užtikrina aukščiausią gaminių kokybę ir greitą bei skalndų
-            užsakymų įgyvendinimą. Pas mus rasite didelį aukščiausios kokybės skardų bei kitų
-            medžiagų pasirinkimą, užtikrinantį šimtaprocentinį jūsų poreikių įgyvendinimą.
-          </p>
-          <div class="flex gap-2 md:gap-4 flex-col md:max-w-96">
-            <div class="md:text-2xl text-xl font-bold">Susisiekite su mumis</div>
-            <p class="text-justify text-sm font-normal">
-              Atsakysime į visus jums rūpimus klausimus, pakonsultuosime ir pateiksime individualų
-              pasiūlymą, atitinkantį jūsų poreikius.
+      <NuxtImg
+        src="/images/stipriausi.jpg"
+        alt="Rombo tipo tvora"
+        decoding="auto"
+        loading="lazy"
+        :ismap="true"
+        class="object-contain rounded-md self-center w-full h-auto hidden md:block lg:hidden min-w-0"
+      />
+      <div class="flex gap-4">
+        <div class="flex flex-col gap-4 flex-1 justify-between">
+          <NuxtImg
+            src="/images/stipriausi.jpg"
+            alt="Rombo tipo tvora"
+            decoding="auto"
+            loading="lazy"
+            :ismap="true"
+            class="object-contain rounded-md w-full h-auto md:hidden lg:block"
+          />
+          <div class="flex flex-col gap-4 items-center justify-evenly flex-1">
+            <p class="text-justify font-normal md:max-w-96 min-w-72">
+              <span class="font-bold sm:text-xl text-lg">„Moderni Tvora“</span>
+              – šiuolaikiškus tvorų sprendimus siūlanti įmonė. Ilgametę patirtį
+              turinti profesionalų komanda specializuojasi skardinių tvorų
+              gamyboje. Didelį dėmesį skiria gaminių kokybei bei valdymo
+              procesams, kurie užtikrina aukščiausią gaminių kokybę ir greitą
+              bei skalndų užsakymų įgyvendinimą. Pas mus rasite didelį
+              aukščiausios kokybės skardų bei kitų medžiagų pasirinkimą,
+              užtikrinantį šimtaprocentinį jūsų poreikių įgyvendinimą.
             </p>
-            <div class="flex flex-col gap-2 md:mt-4">
-              <a
-                href="mailto:audrius@modernitvora.lt"
-                class="flex gap-2 hover:scale-105 transition-transform w-fit"
-              >
-                <svg
-                  width="22"
-                  height="22"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  xmlns="http://www.w3.org/2000/svg"
+            <div class="flex gap-2 md:gap-4 flex-col md:max-w-96">
+              <div class="md:text-2xl text-xl font-bold">
+                Susisiekite su mumis
+              </div>
+              <p class="text-justify text-sm font-normal">
+                Atsakysime į visus jums rūpimus klausimus, pakonsultuosime ir
+                pateiksime individualų pasiūlymą, atitinkantį jūsų poreikius.
+              </p>
+              <div class="flex flex-col gap-2 md:mt-4">
+                <a
+                  href="mailto:audrius@modernitvora.lt"
+                  class="flex gap-2 hover:scale-105 transition-transform w-fit"
                 >
-                  <path
-                    d="M17 20.5H7C4 20.5 2 19 2 15.5V8.5C2 5 4 3.5 7 3.5H17C20 3.5 22 5 22 8.5V15.5C22 19 20 20.5 17 20.5Z"
-                    stroke="red"
-                    stroke-width="1.5"
-                    stroke-miterlimit="10"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                  <svg
+                    width="22"
+                    height="22"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      d="M17 20.5H7C4 20.5 2 19 2 15.5V8.5C2 5 4 3.5 7 3.5H17C20 3.5 22 5 22 8.5V15.5C22 19 20 20.5 17 20.5Z"
+                      stroke="red"
+                      stroke-width="1.5"
+                      stroke-miterlimit="10"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                    <path
+                      d="M17 9L13.87 11.5C12.84 12.32 11.15 12.32 10.12 11.5L7 9"
+                      stroke="red"
+                      stroke-width="1.5"
+                      stroke-miterlimit="10"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                    />
+                  </svg>
+                  <p>audrius@modernitvora.lt</p>
+                </a>
+
+                <a
+                  href="tel:+37067517745"
+                  class="flex gap-2 hover:scale-105 transition-transform w-fit"
+                >
+                  <NuxtImg
+                    src="/icons/call.svg"
+                    alt="Moderni Tvora logotipas"
+                    width="20"
+                    height="20"
+                    decoding="auto"
+                    loading="lazy"
+                    :ismap="true"
                   />
-                  <path
-                    d="M17 9L13.87 11.5C12.84 12.32 11.15 12.32 10.12 11.5L7 9"
-                    stroke="red"
-                    stroke-width="1.5"
-                    stroke-miterlimit="10"
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
+                  <p>+370 675 17745</p>
+                </a>
+
+                <NuxtLink
+                  to="https://maps.app.goo.gl/YThusc8NVmJxAMAS9"
+                  target="_blank"
+                  class="flex gap-2 hover:scale-105 transition-transform w-fit"
+                >
+                  <NuxtImg
+                    src="/icons/location.svg"
+                    alt="Moderni Tvora logotipas"
+                    width="20"
+                    height="20"
+                    decoding="auto"
+                    loading="lazy"
+                    :ismap="true"
                   />
-                </svg>
-                <p>audrius@modernitvora.lt</p>
-              </a>
+                  <p>Kauno g. 31, Marijampolė</p>
+                </NuxtLink>
 
-              <a
-                href="tel:+37067517745"
-                class="flex gap-2 hover:scale-105 transition-transform w-fit"
-              >
-                <NuxtImg
-                  src="/icons/call.svg"
-                  alt="Moderni Tvora logotipas"
-                  width="20"
-                  height="20"
-                  decoding="auto"
-                  loading="lazy"
-                  :ismap="true"
-                />
-                <p>+370 675 17745</p>
-              </a>
-
-              <NuxtLink
-                to="https://maps.app.goo.gl/YThusc8NVmJxAMAS9"
-                target="_blank"
-                class="flex gap-2 hover:scale-105 transition-transform w-fit"
-              >
-                <NuxtImg
-                  src="/icons/location.svg"
-                  alt="Moderni Tvora logotipas"
-                  width="20"
-                  height="20"
-                  decoding="auto"
-                  loading="lazy"
-                  :ismap="true"
-                />
-                <p>Kauno g. 31, Marijampolė</p>
-              </NuxtLink>
-
-              <NuxtLink
-                to="https://modernitvora.lt/lt"
-                target="_blank"
-                class="flex gap-2 hover:scale-105 transition-transform w-fit"
-              >
-                <NuxtImg
-                  src="/icons/bank.svg"
-                  alt="Moderni Tvora logotipas"
-                  width="20"
-                  height="20"
-                  decoding="auto"
-                  loading="lazy"
-                  :ismap="true"
-                />
-                <p>Apie mus</p>
-              </NuxtLink>
+                <NuxtLink
+                  to="https://modernitvora.lt/lt"
+                  target="_blank"
+                  class="flex gap-2 hover:scale-105 transition-transform w-fit"
+                >
+                  <NuxtImg
+                    src="/icons/bank.svg"
+                    alt="Moderni Tvora logotipas"
+                    width="20"
+                    height="20"
+                    decoding="auto"
+                    loading="lazy"
+                    :ismap="true"
+                  />
+                  <p>Apie mus</p>
+                </NuxtLink>
+              </div>
             </div>
           </div>
         </div>
@@ -119,7 +131,7 @@
           decoding="auto"
           loading="lazy"
           :ismap="true"
-          class="object-cover object-top max-h-[800px] min-h-[560px] rounded-md self-center flex-1 md:block hidden w-full h-full"
+          class="object-cover object-top rounded-md self-center flex-1 md:block hidden min-w-0"
         />
       </div>
     </div>
--- components/Offers/SteelInfo.vue
+++ components/Offers/SteelInfo.vue
@@ -1,20 +1,30 @@
 <template>
   <OffersBoxWrap>
     <div class="text-2xl font-bold text-center">Skardos informacija</div>
-    <div class="sm:grid grid-cols-[1.2fr_1fr_1fr] border-2 border-black rounded-md hidden">
-      <div class="px-4 py-1 border-b border-inherit border-r">Skardos tipas</div>
-      <div class="px-4 py-1 border-b border-inherit border-r font-bold">Premium</div>
+    <div
+      class="sm:grid grid-cols-[1.2fr_1fr_1fr] border-2 border-black rounded-md hidden"
+    >
+      <div class="px-4 py-1 border-b border-inherit border-r">
+        Skardos tipas
+      </div>
+      <div class="px-4 py-1 border-b border-inherit border-r font-bold">
+        Premium
+      </div>
       <div class="px-4 py-1 border-b border-inherit font-bold">Eco</div>
       <div class="px-4 py-1 border-b border-inherit border-r">Gamintojas</div>
       <div class="px-4 py-1 border-b border-inherit border-r">Thyssenkrupp</div>
       <div class="px-4 py-1 border-b border-inherit">Marcegaglia</div>
-      <div class="px-4 py-1 border-b border-inherit border-r">Gamintojo šalis</div>
+      <div class="px-4 py-1 border-b border-inherit border-r">
+        Gamintojo šalis
+      </div>
       <div class="px-4 py-1 border-b border-inherit border-r">Vokietija</div>
       <div class="px-4 py-1 border-b border-inherit">Italija</div>
       <div class="px-4 py-1 border-b border-inherit border-r">Garantija</div>
       <div class="px-4 py-1 border-b border-inherit border-r">45 metai</div>
       <div class="px-4 py-1 border-b border-inherit">5 metai</div>
-      <div class="px-4 py-1 border-b border-inherit border-r">Skardos storis</div>
+      <div class="px-4 py-1 border-b border-inherit border-r">
+        Skardos storis
+      </div>
       <div class="px-4 py-1 border-b border-inherit border-r">0.5 mm</div>
       <div class="px-4 py-1 border-b border-inherit">0.45 mm</div>
       <div class="px-4 py-1 border-b border-inherit border-r">
@@ -22,17 +32,23 @@
       </div>
       <div class="px-4 py-1 border-b border-inherit border-r">Taip</div>
       <div class="px-4 py-1 border-b border-inherit">Ne</div>
-      <div class="px-4 py-1 border-b border-inherit border-r">Rekomenduojamas elemento ilgis</div>
+      <div class="px-4 py-1 border-b border-inherit border-r">
+        Rekomenduojamas elemento ilgis
+      </div>
       <div class="px-4 py-1 border-b border-inherit border-r">2500 mm</div>
       <div class="px-4 py-1 border-b border-inherit">2000 mm</div>
-      <div class="px-4 py-1 border-b border-inherit border-r">Maksimalus elemento ilgis</div>
+      <div class="px-4 py-1 border-b border-inherit border-r">
+        Maksimalus elemento ilgis
+      </div>
       <div class="px-4 py-1 border-b border-inherit border-r">3000 mm</div>
       <div class="px-4 py-1 border-b border-inherit">2500 mm</div>
-      <div class="px-4 py-1 border-b border-inherit border-r">Metalinės dangos masė (g/m2)</div>
+      <div class="px-4 py-1 border-b border-inherit border-r">
+        Metalinės dangos masė (g/m2)
+      </div>
       <div class="px-4 py-1 border-b border-inherit border-r">
         ZM 130 (cinko, magnio mišinys. Zn atitikmuo 260)
       </div>
-      <div class="px-4 py-1 border-b border-inherit">---</div>
+      <div class="px-4 py-1 border-b border-inherit">Zn 100</div>
       <div class="px-4 py-1 border-r border-inherit">Padengimas</div>
       <div class="px-4 py-1 border-r border-inherit">
         min. 35 mikronų dviejų sluoksnių dengimo sistema
@@ -41,25 +57,39 @@
     </div>
 
     <div class="sm:hidden grid-cols-2 border-2 border-black rounded-md grid">
-      <div class="px-2 py-1 border-b border-inherit border-r">Skardos tipas</div>
-      <div class="px-2 py-1 border-b border-inherit border-r font-bold">Premium</div>
+      <div class="px-2 py-1 border-b border-inherit border-r">
+        Skardos tipas
+      </div>
+      <div class="px-2 py-1 border-b border-inherit border-r font-bold">
+        Premium
+      </div>
       <div class="px-2 py-1 border-b border-inherit border-r">Gamintojas</div>
       <div class="px-2 py-1 border-b border-inherit border-r">Thyssenkrupp</div>
-      <div class="px-2 py-1 border-b border-inherit border-r">Gamintojo šalis</div>
+      <div class="px-2 py-1 border-b border-inherit border-r">
+        Gamintojo šalis
+      </div>
       <div class="px-2 py-1 border-b border-inherit border-r">Vokietija</div>
       <div class="px-2 py-1 border-b border-inherit border-r">Garantija</div>
       <div class="px-2 py-1 border-b border-inherit border-r">45 metai</div>
-      <div class="px-2 py-1 border-b border-inherit border-r">Skardos storis</div>
+      <div class="px-2 py-1 border-b border-inherit border-r">
+        Skardos storis
+      </div>
       <div class="px-2 py-1 border-b border-inherit border-r">0.5 mm</div>
       <div class="px-2 py-1 border-b border-inherit border-r">
         Skardos identiško atspalvio garantas
       </div>
       <div class="px-2 py-1 border-b border-inherit border-r">Taip</div>
-      <div class="px-2 py-1 border-b border-inherit border-r">Rekomenduojamas elemento ilgis</div>
+      <div class="px-2 py-1 border-b border-inherit border-r">
+        Rekomenduojamas elemento ilgis
+      </div>
       <div class="px-2 py-1 border-b border-inherit border-r">2500 mm</div>
-      <div class="px-2 py-1 border-b border-inherit border-r">Maksimalus elemento ilgis</div>
+      <div class="px-2 py-1 border-b border-inherit border-r">
+        Maksimalus elemento ilgis
+      </div>
       <div class="px-2 py-1 border-b border-inherit border-r">3000 mm</div>
-      <div class="px-2 py-1 border-b border-inherit border-r">Metalinės dangos masė (g/m2)</div>
+      <div class="px-2 py-1 border-b border-inherit border-r">
+        Metalinės dangos masė (g/m2)
+      </div>
       <div class="px-2 py-1 border-b border-inherit border-r">
         ZM 130 (cinko, magnio mišinys. Zn atitikmuo 260 g)
       </div>
@@ -70,25 +100,37 @@
     </div>
 
     <div class="sm:hidden grid-cols-2 border-2 border-black rounded-md grid">
-      <div class="px-2 py-1 border-b border-inherit border-r">Skardos tipas</div>
+      <div class="px-2 py-1 border-b border-inherit border-r">
+        Skardos tipas
+      </div>
       <div class="px-2 py-1 border-b border-inherit font-bold">Eco</div>
       <div class="px-2 py-1 border-b border-inherit border-r">Gamintojas</div>
       <div class="px-2 py-1 border-b border-inherit">Marcegaglia</div>
-      <div class="px-2 py-1 border-b border-inherit border-r">Gamintojo šalis</div>
+      <div class="px-2 py-1 border-b border-inherit border-r">
+        Gamintojo šalis
+      </div>
       <div class="px-2 py-1 border-b border-inherit">Italija</div>
       <div class="px-2 py-1 border-b border-inherit border-r">Garantija</div>
       <div class="px-2 py-1 border-b border-inherit">5 metai</div>
-      <div class="px-2 py-1 border-b border-inherit border-r">Skardos storis</div>
+      <div class="px-2 py-1 border-b border-inherit border-r">
+        Skardos storis
+      </div>
       <div class="px-2 py-1 border-b border-inherit">0.45 mm</div>
       <div class="px-2 py-1 border-b border-inherit border-r">
         Skardos identiško atspalvio garantas
       </div>
       <div class="px-2 py-1 border-b border-inherit">Ne</div>
-      <div class="px-2 py-1 border-b border-inherit border-r">Rekomenduojamas elemento ilgis</div>
+      <div class="px-2 py-1 border-b border-inherit border-r">
+        Rekomenduojamas elemento ilgis
+      </div>
       <div class="px-2 py-1 border-b border-inherit">2000 mm</div>
-      <div class="px-2 py-1 border-b border-inherit border-r">Maksimalus elemento ilgis</div>
+      <div class="px-2 py-1 border-b border-inherit border-r">
+        Maksimalus elemento ilgis
+      </div>
       <div class="px-2 py-1 border-b border-inherit">2500 mm</div>
-      <div class="px-2 py-1 border-b border-inherit border-r">Metalinės dangos masė (g/m2)</div>
+      <div class="px-2 py-1 border-b border-inherit border-r">
+        Metalinės dangos masė (g/m2)
+      </div>
       <div class="px-2 py-1 border-b border-inherit">---</div>
       <div class="px-2 py-1 border-r border-inherit">Padengimas</div>
       <div class="px-2 py-1">---</div>
