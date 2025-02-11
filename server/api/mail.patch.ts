78568b502582e76f76c7813274d615671ee4f25c
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2025-02-11 16:16:12 +0200

    asdasd

--- server/api/mail.patch.ts
+++ server/api/mail.patch.ts
@@ -24,7 +24,7 @@ export default defineEventHandler(async (event: any) => {
       <p >Sveiki,</p> 
       <br/>
       <br/>
-      <p>Mes esame vieni didžiausių skardinių tvorų gamintojai Lietuvoje. Specializuojamos populiariausių tvorų modelių gamyboje. 
+      <p>Mes esame vieni didžiausių skardinių tvorų gamintojai Lietuvoje. Specializuojamės populiariausių tvorų modelių gamyboje. 
       Didžiausią dėmesį kreipiame į gaminių kokybę ir pristatymo terminą, kuris reikalui esant gali būti ir 2 darbo dienos.
       Keletas faktų, kodėl verta rinktis mus:</p>
       <br/>
@@ -33,22 +33,23 @@ export default defineEventHandler(async (event: any) => {
             kuri užtikrina gamybos procesą be klaidų.</li>
         <li>Patogumas - tvora gaminama pagal kliento pateiktą tarpų numeraciją ir visi pakai yra supakuojami ir sunumeruojami, 
             jog atitiktų jūsų pateiktą numeraciją ir nereikėtų objekte gaišti laiko atisrenkant, kur kokios lamelės turi dėtis.</li>
-        <li>Lankstumas - tvoros gaminius galime pasiūlyti iš PREMIUM klasės skardos su 45m. garantija ir ECO variantą su 5m. plonesne skarda.</li>
-        <li>Greitis - Gamybos terminas 7 d.d. reikalui esant galim pagaminti per 2 darbo dienas. Taip pat pristatome gaminius visoje Lietuvoje.</li>
+        <li>Lankstumas - tvoros gaminius galime pasiūlyti iš PREMIUM klasės skardos su 45m. garantija ir ECO variantą su 5m. garantija.</li>
+        <li>Greitis - gamybos su pristatymu terminas - 7 darbo dienos, reikalui esant, galim pagaminti per 2 darbo dienas. Taip pat, pristatome gaminius visoje Lietuvoje.</li>
         <li>Inovatyvumas - mes nuolat atnaujiname tiek gaminių dizainą, tiek pačius procesus, jog pagerintume montavimo greitį, gaminių kokybę 
             ir visada atsižvelgiame į mūsų klientų pasiūlymus.</li>
+        <li>Pagalba - į pagalbą galime pasiūlyti vieną iš savo betonuotojų ar montuotojų brigadų.</li>
       </ol>
       <br/>
-      <p>Prašome patyrinėti mūsų didmenos kainyną ir jei nuspręsite pasirinkti mus kaip savo tvorų tiekėją mes dėsime visas pastangas, 
+      <p>Prašome patyrinėti mūsų didmenos kainyną, ir jei nuspręsite pasirinkti mus kaip savo tvorų tiekėją, mes dėsime visas pastangas, 
           kad atitiktume jūsų lūkesčius.</p> 
       <br/>
       <p>Kainininką galite peržiūrėti paspaudę šią nuorodą: <a href="https://modernitvora.vercel.app/didmena" style="font-size: large; font-weight: 500">Kainininkas</a></p> 
       
       <br/>
-      <p> Jei turėsite klausimų nedvejodami kreipkitės.</p>
+      <p> Jei turėsite klausimų, nedvejodami kreipkitės.</p>
       <br/>
       <br/>
-      <a href="https://modernitvora.vercel.app/didmena/atsisakyti/${recipient._id}" style="font-size: large; font-weight: 500">Atsisakyti naujienlaikraščio</a>
+      <a href="https://modernitvora.vercel.app/didmena/atsisakyti/${recipient._id}" style="font-size: large; font-weight: 500">Atsisakyti naujienlaikraščio prenumeratos</a>
   </body>
     </html>
   `;
