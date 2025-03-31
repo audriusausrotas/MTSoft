e93d989fc1705e59dbee1ee06575e703c9553169
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2025-03-31 11:41:10 +0300

    general information updated

--- pages/pasiulymas/[id]/bendra_informacija.vue
+++ pages/pasiulymas/[id]/bendra_informacija.vue
@@ -7,33 +7,69 @@ definePageMeta({
 <template>
   <div class="flex flex-col gap-8 py-8">
     <div class="flex flex-col gap-8">
-      <h2 class="text-2xl font-semibold bg-red-full rounded-xl p-3 text-white text-center">
+      <h2
+        class="text-2xl font-semibold bg-red-full rounded-xl p-3 text-white text-center"
+      >
         Taisyklės ir Nuostatos
       </h2>
       <ol class="list-decimal text-xl font-semibold pl-10 flex flex-col gap-2">
-        <li>Į senos tvoros ar stulpų demontavimo kainą atliekų išvežimas bei gerbūvio tvarkymo darbai neįtraukti</li>
-        <li>Į stulpų betonavimo kainą atliekų išvežimas bei gerbūvio tvarkymo darbai neįtraukti</li>
-        <li>Stulpų betonavimo kaina gali kisti, dėl sudėtingo gręžimo, kurio neįmanoma įsivertinti skaičiuojant sąmatą
+        <li>
+          Į senos tvoros ar stulpų demontavimo kainą atliekų išvežimas bei
+          gerbūvio tvarkymo darbai neįtraukti
         </li>
-        <li>Klientui pageidaujant atlikti gerbūvio darbus ar išvežti atliekas, taikomas papildomas mokestis, kuris
-          aptariamas su klientu</li>
-        <li> Dėl papildomų darbų atlikimo, klientas turi kreiptis į atsakingą vadybininką</li>
-        <li>Darbų atlikimo terminai gali kisti dėl sudėtingo gręžimo ar "force majeure" aplinkybių</li>
-        <li>Klientas, patvirtindamas pasiūlymą, patvirtina, jog pasirinktas tvoros tipas, spalva, naudojamos detalės jį
-          tenkina. Jei po darbų atlikimo klientas norės kitokios tvoros tipo, spalvos ar detalių nei nurodyta
-          patvirtintame pasiūlyme, juos galima keisti papildomai sumokant už naujas medžiagas bei montavimo ir
-          demontavimo darbus</li>
-        <li>Moderni Tvora turi teisę perskaičiuoti medžiagų bei darbų kainas, praėjus ilgesniam nei 3 mėnesių
-          laikotarpiui nuo užsakymo patvirtinimo, kai darbai nebuvo atliekami dėl kliento kaltės</li>
-        <li>Klientui atšaukus patvirtintą užsakymą, užstatas yra negrąžinamas</li>
-        <li>Klientas, patvirtindamas pasiūlymą, sutinka su darbų bei medžiagų kainomis ir neturi teisės prašyti
-          papildomų nuolaidų po darbų atlikimo</li>
-        <li>Klientas, patvirtindamas pasiūlymą, patvirtina, jog susipažino su visomis sąlygomis, taisyklėmis bei
-          privatumo politika</li>
+        <li>
+          Į stulpų betonavimo kainą atliekų išvežimas bei gerbūvio tvarkymo
+          darbai neįtraukti
+        </li>
+        <li>
+          Stulpų betonavimo kaina gali kisti, dėl sudėtingo gręžimo, kurio
+          neįmanoma įsivertinti skaičiuojant sąmatą
+        </li>
+        <li>
+          Klientui pageidaujant atlikti gerbūvio darbus ar išvežti atliekas,
+          taikomas papildomas mokestis, kuris aptariamas su klientu
+        </li>
+        <li>
+          Dėl papildomų darbų atlikimo, klientas turi kreiptis į atsakingą
+          vadybininką
+        </li>
+        <li>
+          Darbų atlikimo terminai gali kisti dėl sudėtingo gręžimo ar "force
+          majeure" aplinkybių
+        </li>
+        <li>
+          Tvoros sąmata yra preliminari. Sąmata perskaičiuojama pagal faktą po
+          stulpų subetonavimo, kai žinomi tikslūs atstumai tarp stulpų.
+        </li>
+        <li>
+          Klientas, patvirtindamas pasiūlymą, patvirtina, jog pasirinktas tvoros
+          tipas, spalva, naudojamos detalės jį tenkina. Jei po darbų atlikimo
+          klientas norės kitokios tvoros tipo, spalvos ar detalių nei nurodyta
+          patvirtintame pasiūlyme, juos galima keisti papildomai sumokant už
+          naujas medžiagas bei montavimo ir demontavimo darbus
+        </li>
+        <li>
+          Moderni Tvora turi teisę perskaičiuoti medžiagų bei darbų kainas,
+          praėjus ilgesniam nei 3 mėnesių laikotarpiui nuo užsakymo
+          patvirtinimo, kai darbai nebuvo atliekami dėl kliento kaltės
+        </li>
+        <li>
+          Klientui atšaukus patvirtintą užsakymą, užstatas yra negrąžinamas
+        </li>
+        <li>
+          Klientas, patvirtindamas pasiūlymą, sutinka su darbų bei medžiagų
+          kainomis ir neturi teisės prašyti papildomų nuolaidų po darbų atlikimo
+        </li>
+        <li>
+          Klientas, patvirtindamas pasiūlymą, patvirtina, jog susipažino su
+          visomis sąlygomis, taisyklėmis bei privatumo politika
+        </li>
       </ol>
     </div>
     <div class="flex flex-col gap-8">
-      <h2 class="text-2xl font-semibold bg-red-full rounded-xl p-3 text-white text-center">
+      <h2
+        class="text-2xl font-semibold bg-red-full rounded-xl p-3 text-white text-center"
+      >
         Eiga
       </h2>
       <ol class="list-decimal text-xl font-semibold pl-10 flex flex-col gap-2">
@@ -54,11 +90,16 @@ definePageMeta({
           Pamatėlių dėjimas. Sustingus betonui (po 7-10 dienų) sudedami
           pamatėliai. Pamatėlių dėjimui reikalinga elektra
         </li>
+        <li>
+          Tikslių atstumų tarp stulpų matavimas, sąmatos patikslinimas pagal
+          faktą
+        </li>
         <li>Tvoros montavimas. Montavimui reikalinga elektra</li>
         <li>Vartų montavimas. Vartų montavimui reikalinga elektra</li>
         <li>Vartų užpildo montavimas. Sumontavus vartus, sudedame užpildą</li>
         <li>
-          Sąmatos perskaičiavimas pagal faktą ir atsiskaitymas už atliktus darbus
+          Sąmatos perskaičiavimas pagal faktą ir atsiskaitymas už atliktus
+          darbus
         </li>
       </ol>
     </div>
