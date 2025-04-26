bd66c3e6cc2ff559d161560d1cafeb39406a5e72
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2025-04-26 09:57:39 +0300

    asdasd

--- middleware/auth.global.ts
+++ middleware/auth.global.ts
@@ -42,7 +42,19 @@ export default defineNuxtRouteMiddleware(async (to) => {
   if (!user) {
     const response = await fetchUser();
     if (!response.success) {
-      const logoutResponse = await request.get("logout");
+      // const logoutResponse = await request.get("logout");
+      const isDevelopment = process.env.NODE_ENV === "development";
+      let logoutResponse: any;
+
+      if (isDevelopment) {
+        logoutResponse = await request.get("logout");
+      } else {
+        const options: any = {
+          method: "GET",
+          credentials: "include",
+        };
+        logoutResponse = await $fetch(`https://mtsoft.lt/api/logout`, options);
+      }
       if (logoutResponse.success) userStore.logout();
 
       if (to.path !== "/login") return navigateTo("/login");
