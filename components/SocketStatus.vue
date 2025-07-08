fd7fbf2a9221aef0828bf6bf11b582767c520587
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2025-07-08 15:26:40 +0300

    interneto vizija suuucks

--- components/SocketStatus.vue
+++ components/SocketStatus.vue
@@ -4,8 +4,18 @@ const props = defineProps(["connected", "transporter"]);
 
 <template>
   <div class="absolute top-1 right-1 flex gap-1">
-    <div :class="['w-2 h-2 rounded-full ', connected ? 'bg-green-500' : 'bg-red-600']"></div>
-    <div :class="['w-2 h-2 rounded-full ', transporter ? 'bg-green-500' : 'bg-red-600']"></div>
+    <div
+      :class="[
+        'w-2 h-2 rounded-full ',
+        props.connected ? 'bg-green-500' : 'bg-red-600',
+      ]"
+    ></div>
+    <div
+      :class="[
+        'w-2 h-2 rounded-full ',
+        props.transporter ? 'bg-green-500' : 'bg-red-600',
+      ]"
+    ></div>
   </div>
 </template>
 
