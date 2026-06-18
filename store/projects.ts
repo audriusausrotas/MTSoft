5e6d52b89a5178b06ec5da12786c3bb61ae0e3a5
Audrius <audrius@modernitvora.lt>
2026-06-18 22:17:12 +0300

    asdf

--- store/projects.ts
+++ store/projects.ts
@@ -96,13 +96,6 @@ export const useProjectsStore = defineStore("Projects", {
       const project = this.projects.find((p) => p._id === _id);
       if (project) project.dates.dateCompletion = date;
     },
-
-    // aditionalOrderedProject(_id: string, data: Fence[]) {
-    //   const project = this.projects.find((p) => p._id === _id);
-    //   if (project) {
-    //     project.fences = [...project.fences, ...data];
-    //   }
-    // },
   },
 
   getters: {
