d681e487e330479d171f703582632b1522518bf5
Audrius Aušrotas <audrius.ausrotas@gmail.com>
2026-07-24 10:13:49 +0300

    fixing stuff

--- pages/Ekranas.vue
+++ pages/Ekranas.vue
@@ -6,17 +6,35 @@ definePageMeta({
 
 const useSocket = useSocketStore();
 
+async function logoutHandler() {
+  const response: any = await request.get("logout");
+  if (response.success) {
+    const userStore = useUserStore();
+    userStore.logout();
+    const router = useRouter();
+    router.push("/login");
+  }
+}
+
 onMounted(async () => {
   useSocket.connect();
 });
 </script>
 
 <template>
-  <div class="flex flex-col h-screen w-screen p-8 items-center gap-14 relative">
+  <div
+    class="flex flex-col h-screen w-screen p-8 items-center gap-14 relative select-none"
+  >
     <ClientOnly>
       <ScreenTime />
     </ClientOnly>
     <ScreenReports />
+    <div
+      @click="logoutHandler"
+      class="absolute top-1 left-1 border border-gray-300 rounded-lg shadow-lg p-1.5 w-8 h-8 hover:cursor-pointer"
+    >
+      <img src="/icons/logout.svg" alt="logout button" />
+    </div>
   </div>
 </template>
 <style scoped></style>
